684b1edc298a982753e8fce793b1c8ed1355b32c
MichelangeloCaltagirone <michelangelocaltagirone@gmail.com>
2024-09-18 15:57:46 +0200

    Preparing Scaffolding

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,28 @@
+<script>
+    export default {
+        data() {
+            return {
+
+            }
+        }
+    }
+</script>
+
+<template>
+
+    <div class="containerCustom">
+        <img src="" alt="logo yugi">
+        <h1>Yu-Gi-Oh API</h1>
+    </div>
+
+</template>
+
+
+<style scoped>
+
+.containerCustom {
+    max-width: 1100px;
+    margin: 0 auto;
+}
+
+</style>
