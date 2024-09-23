e9b57b987842b4f69374e3ab5710f9832b2097aa
MichelangeloCaltagirone <michelangelocaltagirone@gmail.com>
2024-09-23 16:28:58 +0200

    Add more style to the page. Esercizio finito.

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,7 +2,6 @@
     export default {
         data() {
             return {
-
             }
         }
     }
@@ -11,7 +10,7 @@
 <template>
 
     <div class="containerCustom">
-        <img src="" alt="logo yugi">
+        <img id="logoHeader" src="../assets/img/Yu-Gi-Ohlogo.png" alt="logo yugi">
         <h1>Yu-Gi-Oh API</h1>
     </div>
 
@@ -20,9 +19,22 @@
 
 <style scoped>
 
+#logoHeader {
+    width: 100px;
+    position: absolute;
+    top: -35;
+    left: -105px;
+}
 .containerCustom {
     max-width: 1100px;
     margin: 0 auto;
+    padding: 2rem 0 2rem 10px;
+    position: relative;
+
+    h1 {
+        display: inline;
+        font-size: 30px
+    }
 }
 
 </style>
