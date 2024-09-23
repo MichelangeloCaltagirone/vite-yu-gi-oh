19235aaa22142627f41de4fc9ec5ade1a6b770e6
MichelangeloCaltagirone <michelangelocaltagirone@gmail.com>
2024-09-23 11:38:38 +0200

    Add different components for the initial correct behavior

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,9 +1,49 @@
 <script>
+ export default {
+        data() {
+            return {
+
+            }
+        },
+        props: {
+            picUrl: {
+                type: String,
+                required: true
+            },
+            archetype: {
+                type: String,
+                required: false
+            },
+            name: {
+                type: String,
+                required: true
+            }
+        },
+        methods: { 
+            getImageUrl(picPath) {
+                return new URL(`../assets/img/${picPath}`, import.meta.url).href
+            }
+        }
+    
+    }
 </script>
 
 <template>
 
+    <article>
+        <img :src="picUrl" :alt="name">
+        <div>{{ name }}</div>
+        <div>{{ archetype }}</div>
+    </article>
+
 </template>
 
 <style scoped>
+article {
+    display: inline;
+    flex-basis: calc(100% / 5);
+    img {
+        height: 250px;
+    }
+}
 </style>
